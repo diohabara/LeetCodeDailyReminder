35c627f2e4b7045f5f99eda776fe48d9df88abaa
tuampon <diohabara@gmail.com>
2022-12-04 23:01:24 -0600

    feat: use api v2

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
-const LEETCODE_API_ENDPOINT = "https://leetcode.com/graphql";
-const TODOIST_API_ENDPOINT = "https://api.todoist.com/rest/v1";
-const TODOIST_API_TOKEN = Deno.env.get("TODOIST_API_TOKEN");
+import { TodoistApi } from "npm:@doist/todoist-api-typescript";
+
 const DAILY_CODING_CHALLENGE_QUERY = `
 query questionOfToday {
 	activeDailyCodingChallengeQuestion {
@@ -27,6 +26,13 @@ query questionOfToday {
 		}
 	}
 }`;
+
+const LEETCODE_API_ENDPOINT = "https://leetcode.com/graphql";
+const TODOIST_API_TOKEN = Deno.env.get("TODOIST_API_TOKEN");
+if (TODOIST_API_TOKEN === undefined) {
+  console.error("TODOIST_API_TOKEN is not set.");
+  Deno.exit(1);
+}
 
 interface Question {
   data: {
@@ -66,38 +72,55 @@ const fetchDailyCodingChallenge = async () => {
     body: JSON.stringify({ query: DAILY_CODING_CHALLENGE_QUERY }),
   };
 
+  console.log("Fetching json...");
   const response = await fetch(LEETCODE_API_ENDPOINT, init);
+  console.log("Successfully fetched\n", response);
   return response.json();
 };
 
 // Passing in the `question` object from fetchDailyCodingChallenge function
 const createTodoistTask = async (question: Question) => {
   const questionInfo = question.data.activeDailyCodingChallengeQuestion;
-
   const questionTitle = questionInfo.question.title;
   const questionDifficulty = questionInfo.question.difficulty;
   const questionLink = `https://leetcode.com${questionInfo.link}`;
-
-  console.log(`Creating Todoist task with title ${questionTitle}.`);
-
+  const questionId = questionInfo.question.frontendQuestionId;
+  const api = new TodoistApi(TODOIST_API_TOKEN);
+  let targetSectionId = "";
+  await api.getSections()
+    .then((sections) => {
+      console.log("Fetching sections...");
+      console.log(sections);
+      for (const section of sections) {
+        if (section.name === "WIP") {
+          targetSectionId = section.id.toString();
+        }
+      }
+      console.log("Successfully fetched:", targetSectionId);
+    })
+    .catch((error) => {
+      console.log(error);
+      Deno.exit(1);
+    });
   const body = {
-    content: `${questionTitle}: [${questionTitle}](${questionLink}) @work`,
+    content:
+      `${questionId}: ${questionTitle} [${questionTitle}](${questionLink})`,
     description: `Difficulty: ${questionDifficulty}`,
-    due_string: "Today",
-    priority: 4,
+    due_string: "today",
+    labels: ["work"],
+    section_id: targetSectionId,
   };
-
-  const init = {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${TODOIST_API_TOKEN}`, // Set at environment variable
-    },
-  };
-
-  const response = await fetch(`${TODOIST_API_ENDPOINT}/tasks`, init);
-  return response.json();
+  console.log(questionInfo);
+  console.log(`Adding Todoist task with title ${questionTitle}...`);
+  await api.addTask(body)
+    .then((project) => {
+      console.log(project);
+      console.log("Successfully created task", project);
+    })
+    .catch((error) => {
+      console.log(error);
+      Deno.exit(1);
+    });
 };
 
 const main = async () => {
