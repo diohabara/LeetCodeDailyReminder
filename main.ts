53b200cad3c13125f9ebcf860596f94b00873a46
tuampon <diohabara@gmail.com>
2022-12-05 17:00:15 -0600

    refactor: change api calls

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { TodoistApi } from "npm:@doist/todoist-api-typescript";
-
+const LEETCODE_API_ENDPOINT = "https://leetcode.com/graphql";
 const DAILY_CODING_CHALLENGE_QUERY = `
 query questionOfToday {
 	activeDailyCodingChallengeQuestion {
@@ -26,14 +26,6 @@ query questionOfToday {
 		}
 	}
 }`;
-
-const LEETCODE_API_ENDPOINT = "https://leetcode.com/graphql";
-const TODOIST_API_TOKEN = Deno.env.get("TODOIST_API_TOKEN");
-if (TODOIST_API_TOKEN === undefined) {
-  console.error("TODOIST_API_TOKEN is not set.");
-  Deno.exit(1);
-}
-
 interface Question {
   data: {
     activeDailyCodingChallengeQuestion: {
@@ -62,32 +54,28 @@ interface Question {
   };
 }
 
-// We can pass the JSON response as an object to our createTodoistTask later.
 const fetchDailyCodingChallenge = async () => {
   console.log(`Fetching daily coding challenge from LeetCode API.`);
-
   const init = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ query: DAILY_CODING_CHALLENGE_QUERY }),
   };
-
   console.log("Fetching json...");
   const response = await fetch(LEETCODE_API_ENDPOINT, init);
   console.log("Successfully fetched\n", response);
   return response.json();
 };
 
-// Passing in the `question` object from fetchDailyCodingChallenge function
-const createTodoistTask = async (question: Question) => {
+const createTodoistTask = async (api: TodoistApi, question: Question) => {
   const questionInfo = question.data.activeDailyCodingChallengeQuestion;
   const questionTitle = questionInfo.question.title;
   const questionDifficulty = questionInfo.question.difficulty;
   const questionLink = `https://leetcode.com${questionInfo.link}`;
   const questionId = questionInfo.question.frontendQuestionId;
-  const api = new TodoistApi(TODOIST_API_TOKEN);
   let targetSectionId = "";
-  await api.getSections()
+  await api
+    .getSections()
     .then((sections) => {
       console.log("Fetching sections...");
       console.log(sections);
@@ -112,9 +100,9 @@ const createTodoistTask = async (question: Question) => {
   };
   console.log(questionInfo);
   console.log(`Adding Todoist task with title ${questionTitle}...`);
-  await api.addTask(body)
+  await api
+    .addTask(body)
     .then((project) => {
-      console.log(project);
       console.log("Successfully created task", project);
     })
     .catch((error) => {
@@ -124,8 +112,14 @@ const createTodoistTask = async (question: Question) => {
 };
 
 const main = async () => {
+  const TODOIST_API_TOKEN = Deno.env.get("TODOIST_API_TOKEN");
+  if (TODOIST_API_TOKEN === undefined) {
+    console.error("TODOIST_API_TOKEN is not set.");
+    Deno.exit(1);
+  }
   const question = await fetchDailyCodingChallenge();
-  createTodoistTask(question);
+  const api = new TodoistApi(TODOIST_API_TOKEN);
+  createTodoistTask(api, question);
 };
 
 main();
